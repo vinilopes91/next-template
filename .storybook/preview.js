c9af92dd16452d86e42f952a5bb6fdaa98d7a9bc
vinilopes91 <vinilopes13@hotmail.com>
2021-03-18 23:22:13 -0300

    Adicioand addon do storybook para o router do next

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,12 @@
-// import { addDecorator } from '@storybook/react'
-// import { withNextRouter } from 'storybook-addon-next-router'
+import { addDecorator } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router'
 import GlobalStyles from '../src/styles/global'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-// addDecorator(withNextRouter())
+addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
